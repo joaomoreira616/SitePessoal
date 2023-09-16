a86edc1133ed98d5819dd6b77f062ee58474c039
João Vitor Moreira Mariano da Silva <105250845+joaomoreira616@users.noreply.github.com>
2023-09-16 13:07:34 -0300

    Modal com style e js

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,6 @@
-let fechar = document.querySelector("fechar")
+let fecharsobremim = document.querySelector(".fecharsobremim")
+let fecharamigos = document.querySelector(".fecharamigos")
+let fecharhobbies = document.querySelector(".fecharhobbies")
 let modalinfosobremim = document.querySelector(".modal-infosobremim")
 let modalamigos = document.querySelector(".modal-amigos")
 let modalhobbies = document.querySelector(".modal-hobbies")
@@ -6,29 +8,37 @@ let clicksobremim = document.querySelector(".sobre-mim")
 let clickamigos = document.querySelector(".amigos")
 let clickhobbies = document.querySelector(".hobbies")
 
-clicksobremim.addEventListener("focus", ()=>{
-        modalamigos.style.bottom = "77rem"
-        modalhobbies.style.bottom = "115rem"
+
+// abrir modal sobre mim
+clicksobremim.addEventListener("click", ()=>{
+        modalamigos.style.bottom = "77rem";
+        modalhobbies.style.bottom = "115rem";
         modalinfosobremim.style.bottom = "-1rem";
-        console.log("sobre mim")
-    });
+});
 
+// abrir modal amigos
 clickamigos.addEventListener("focus", ()=>{
-    modalinfosobremim.style.bottom="39rem"
-    modalhobbies.style.bottom = "115rem"
+    modalinfosobremim.style.bottom="39rem";
+    modalhobbies.style.bottom = "115rem";
     modalamigos.style.bottom = "37rem";
-    console.log("oi amigos")
 })
 
+// abrir modal hobbies
 clickhobbies.addEventListener("focus", ()=>{
-    modalinfosobremim.style.bottom = "39rem"
-    modalamigos.style.bottom = "77rem"
+    modalinfosobremim.style.bottom = "39rem";
+    modalamigos.style.bottom = "77rem";
     modalhobbies.style.bottom = "75rem";
-    console.log("oi hobbies")
 })
 
-img_bg.addEventListener("focus", ()=>{
-    modalinfosobremim.style.bottom = "39rem"
-    modalamigos.style.bottom = "77rem"
-    modalhobbies.style.bottom = "115rem"
+fecharsobremim.addEventListener("click", ()=>{
+    modalinfosobremim.style.bottom = "39rem";
+    console.log("teste fechar sobre mim");
+})
+
+fecharamigos.addEventListener("click", ()=>{
+    modalamigos.style.bottom = "77rem";
+})
+
+fecharhobbies.addEventListener("click", ()=>{
+    modalhobbies.style.bottom = "115rem";
 })
