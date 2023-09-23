0cdc154e3d63a403883b9359ac9036d0ee649e48
João Vitor Moreira Mariano da Silva <105250845+joaomoreira616@users.noreply.github.com>
2023-09-23 00:16:54 -0300

    Código dos modais encurtados

--- assets/main.js
+++ assets/main.js
@@ -1,34 +1,24 @@
 let fecharsobremim = document.querySelector(".fecharsobremim")
 let fecharamigos = document.querySelector(".fecharamigos")
-let fecharhobbies = document.querySelector(".fecharhobbies")
 let modalinfosobremim = document.querySelector(".modal-infosobremim")
 let modalamigos = document.querySelector(".modal-amigos")
-let modalhobbies = document.querySelector(".modal-hobbies")
 let clicksobremim = document.querySelector(".sobre-mim")
 let clickamigos = document.querySelector(".amigos")
-let clickhobbies = document.querySelector(".hobbies")
 
 
 // abrir modal sobre mim
 clicksobremim.addEventListener("click", ()=>{
     modalamigos.style.bottom = "90rem";
-    modalhobbies.style.bottom = "132rem";
     modalinfosobremim.style.bottom = "0";
 });
 
 // abrir modal amigos
 clickamigos.addEventListener("focus", ()=>{
     modalinfosobremim.style.bottom="45rem";
-    modalhobbies.style.bottom = "132rem";
     modalamigos.style.bottom = "44rem";
 })
 
-// abrir modal hobbies
-clickhobbies.addEventListener("focus", ()=>{
-    modalinfosobremim.style.bottom = "45rem";
-    modalamigos.style.bottom = "90rem";
-    modalhobbies.style.bottom = "88rem";
-})
+
 
 fecharsobremim.addEventListener("click", ()=>{
     modalinfosobremim.style.bottom = "45rem";
@@ -36,8 +26,4 @@ fecharsobremim.addEventListener("click", ()=>{
 
 fecharamigos.addEventListener("click", ()=>{
     modalamigos.style.bottom = "90rem";
-})
-
-fecharhobbies.addEventListener("click", ()=>{
-    modalhobbies.style.bottom = "132rem";
 })
